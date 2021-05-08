2cc87a717e64a8d650b50ef72a3389bd225acaba
Maros Korinek <maros.korinek@skyscanner.net>
2021-05-08 11:28:25 +0100

    add env vars to docs + nitpics

---
+++ README.md
@@ -0,0 +1,10 @@
+# Budget Desk
+
+## ENV Variables
+
+https://developers.monzo.com/
+
+- `MONZO_CLIENT_ID` 
+- `MONZO_CLIENT_SECRET` 
+- `MONZO_ACC_ID`
+- ``
--- app/util.py
+++ app/util.py
@@ -7,7 +7,7 @@ def str_to_float(s: str):
     return float(s.replace(' ', ''))
 
 
-# 2021-03-04T00:00:00Z
+# example 2021-03-04T00:00:00Z
 def _day_to_daytime_str(date, is_end=False):
     time = "23:59:59Z" if is_end else "00:00:00Z"
     return datetime(date.year, date.month, date.day)\
