a33b68b5ee391dffce421f88ee15735cbec025f5
Maros Korinek <maros.korinek@skyscanner.net>
2021-11-10 17:23:57 +0000

    Be more precise - use InfluxDb

--- app/db/db_client.py
+++ app/db/db_client.py
@@ -2,10 +2,9 @@ import os
 import typing as t
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
-from pandas import DataFrame
 
 
-class DbClient(object):
+class InfluxDbClient(object):
     def __init__(self, token: str, org: str, bucket: str, url: str):
         self.token = token
         self.org = org
@@ -13,19 +12,6 @@ class DbClient(object):
         self._inner: InfluxDBClient = InfluxDBClient(url=url, token=token, org=org, debug=True)
         self.write_client = self._inner.write_api(write_options=SYNCHRONOUS)
         self.read_client = self._inner.query_api()
-
-    def write_dataframe(
-        self,
-        df: DataFrame,
-        tag_columns: t.List[str],
-        measurement_name: str
-    ):
-        self.write_client.write(
-            bucket=self.bucket,
-            record=df,
-            data_frame_tag_columns=tag_columns,
-            data_frame_measurement_name=measurement_name
-        )
 
     def write_records(self, points: t.List[Point]):
         for point in points:
@@ -37,15 +23,12 @@ class DbClient(object):
             record=point,
         )
 
-    def query_df(self, query: str):
-        return self.read_client.query_data_frame(query)
-
     def query(self, query: str):
         return self.read_client.query(query)
 
 
-def build_db_client() -> DbClient:
-    return DbClient(
+def build_db_client() -> InfluxDbClient:
+    return InfluxDbClient(
         token=os.environ.get('INFLUXDB_TOKEN'),
         org=os.environ.get('INFLUXDB_ORG'),
         bucket=os.environ.get('INFLUXDB_BUCKET'),
