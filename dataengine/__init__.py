d38ebc19545ebb2f232e8f9f986f1019ba735bff
Maros Korinek <maros.korinek@skyscanner.net>
2021-12-11 14:32:31 +0000

    Register auth0 during init

--- dataengine/__init__.py
+++ dataengine/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 from dataengine.config import SERVER_SECRET_KEY, SERVER_SESSION_TYPE
+from authlib.integrations.flask_client import OAuth
+from config import AUTH0_CLIENT_ID, AUTO0_CLIENT_SECRET, AUTH0_API_BASE_URL, AUTH0_ACCESS_TOKEN_URL, \
+    AUTH0_AUTHORIZE_URL, AUTH0_CLIENT_KWARGS
+from context import Context
 
 
 def create_app():
@@ -17,6 +21,22 @@ def create_app():
 
     from dataengine.routes.auth import auth_bp
     flask_app.register_blueprint(auth_bp)
+
+
+    oauth = OAuth(flask_app)
+    auth0 = oauth.register(
+        'auth0',
+        client_id=AUTH0_CLIENT_ID,
+        client_secret=AUTO0_CLIENT_SECRET,
+        api_base_url=AUTH0_API_BASE_URL,
+        access_token_url=AUTH0_ACCESS_TOKEN_URL,
+        authorize_url=AUTH0_AUTHORIZE_URL,
+        client_kwargs={
+            'scope': AUTH0_CLIENT_KWARGS,
+        },
+    )
+
+    Context.set_context(flask_app, auth0)
 
     return flask_app
 
