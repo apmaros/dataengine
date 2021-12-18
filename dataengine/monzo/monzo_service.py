93536dac9097c76ee79e3a5f216275dd2e0adba3
Maros Korinek <maros.korinek@skyscanner.net>
2021-12-18 20:28:29 +0000

    Remove obsolete error log

--- dataengine/monzo/monzo_service.py
+++ dataengine/monzo/monzo_service.py
@@ -45,7 +45,6 @@ class MonzoService:
         # TODO Should not swallow exception
         except ApiError as e:
             logger.error(f"Failed to load transactions due to ApiError: {e}")
-            logger.error(f"Monzo API failed to load transactions due to {e}")
             # reload token - might have been evicted by rotation
             if e.is_unauthorised():
                 self._monzo_client.login(load_monzo_token())
