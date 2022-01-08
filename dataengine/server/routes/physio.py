013563544d4d4e0218af5c1cefce553001e02ee6
Maros Korinek <maros.korinek@skyscanner.net>
2022-01-08 09:28:21 +0000

    Fix: POST args are in req form

--- dataengine/server/routes/physio.py
+++ dataengine/server/routes/physio.py
@@ -17,10 +17,10 @@ physio_bp = Blueprint('physio', __name__, url_prefix='/physio')
 @physio_bp.route('/blood_pressure', methods=['POST'])
 @requires_auth
 def blood_pressure():
-    systolic = int(request.args.get("systolic"))
-    diastolic = int(request.args.get("diastolic"))
-    heart_rate = int(request.args.get("heart-rate"))
-    last_activity = request.args.get("last-activity")
+    systolic = int(request.form.get("systolic"))
+    diastolic = int(request.form.get("diastolic"))
+    heart_rate = int(request.form.get("heart-rate"))
+    last_activity = request.form.get("last-activity")
 
     flash("Recorded blood pressure reading ("
           f"blood pressure: {systolic}/{diastolic}, heart rate: {heart_rate})")
