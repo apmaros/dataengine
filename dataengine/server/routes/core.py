182d8e10915c1225730e26389b37bb31fd933d95
Maros Korinek <apmaros@gmail.com>
2022-01-27 21:37:00 +0000

    Display events on main page

--- dataengine/server/routes/core.py
+++ dataengine/server/routes/core.py
@@ -4,9 +4,10 @@ from flask import (
     session,
 )
 
+from dataengine.config import DEFAULT_DISPLAY_RESOURCE_DAYS_AGO
 from dataengine.server.routes.annotations import requires_auth
 from dataengine.service.day_note import get_day_notes
-from dataengine.service.db.event import get_events
+from dataengine.service.db.event import get_events_since
 
 core_bp = Blueprint('core', __name__)
 
@@ -15,7 +16,7 @@ core_bp = Blueprint('core', __name__)
 @requires_auth
 def index():
     profile = session['profile']
-    events = get_events(profile['user_id'])
+    events = get_events_since(profile['user_id'], DEFAULT_DISPLAY_RESOURCE_DAYS_AGO)
     day_notes = get_day_notes(profile['user_id'], start='-7d')
     return render_template('home.html', user_profile=profile, events=events, day_notes=day_notes)
 
