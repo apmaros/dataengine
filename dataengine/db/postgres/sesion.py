8f0cbe81a299064f9bddbd6ca883ef71b4e09657
Maros Korinek <maros.korinek@skyscanner.net>
2022-01-28 11:28:09 +0000

    feat(psql): Recycle connections after 1 hour

--- dataengine/db/postgres/sesion.py
+++ dataengine/db/postgres/sesion.py
@@ -7,15 +7,18 @@ from dataengine.db.postgres.config import DbConfig, get_db_url
 
 
 def get_session(config: DbConfig) -> Session:
-    logger.info(f'creating db connection '
-                f'database={config.database} '
-                f'address={config.host}:{config.port}')
+    logger.debug(
+        f'creating db connection '
+        f'database={config.database} '
+        f'address={config.host}:{config.port}'
+    )
 
     url = get_db_url(config)
     engine = create_engine(
         url,
         echo=config.echo_queries,
-        pool_size=10
+        pool_size=10,
+        pool_recycle=3600
     )
     session_class = sessionmaker(bind=engine)
     return session_class()
