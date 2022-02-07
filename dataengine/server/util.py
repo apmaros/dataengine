f2fe8aa63f29ffe16118e5c8f181cd68fbb976e7
Maros Korinek <maros.korinek@skyscanner.net>
2022-02-07 09:19:10 +0000

    Don't render point without value

--- dataengine/server/util.py
+++ dataengine/server/util.py
@@ -13,3 +13,10 @@ def format_datetime(value, fmt=DATETIME_VIEW_FORMAT):
 
 def split_paragraphs(text):
     return text.split('\n')
+
+
+def or_zero(value):
+    if value:
+        return value
+    else:
+        return 'null'
