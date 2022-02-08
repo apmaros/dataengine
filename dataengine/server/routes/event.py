c24a88a457797c2f77b5556f1bad78d90e736836
Maros Korinek <maros.korinek@skyscanner.net>
2022-02-08 08:55:32 +0000

    Update event

--- dataengine/server/routes/event.py
+++ dataengine/server/routes/event.py
@@ -12,7 +12,7 @@ from flask import (
 from dataengine.common.log import logger
 from dataengine.config import DEFAULT_DISPLAY_RESOURCE_DAYS_AGO
 from dataengine.server.routes.annotations import requires_auth
-from dataengine.service.db.event import get_events_since, put_event
+from dataengine.service.db.event import get_events_since, put_event, get_event, update_event
 
 event_bp = Blueprint('event', __name__, url_prefix='/event')
 
@@ -39,3 +39,29 @@ def new():
     flash(f"👌 Event was recorded", 'success')
 
     return make_response(redirect(url_for('event.index')))
+
+
+@event_bp.route('/edit')
+@requires_auth
+def edit():
+    event = get_event(request.args['id'])
+    return make_response(render_template(
+        'event/edit.html',
+        event=event,
+        referrer=request.referrer,
+    ))
+
+
+@event_bp.route('/edit', methods=['POST'])
+@requires_auth
+def edit_post():
+    profile = session['profile']
+    update_event(request.form, profile['user_id'])
+
+    return make_response(redirect(url_for('event.index')))
+
+
+@event_bp.route('/delete')
+@requires_auth
+def delete():
+    pass
--- dataengine/service/db/event.py
+++ dataengine/service/db/event.py
@@ -1,11 +1,20 @@
 import typing
 import uuid
 
-from sqlalchemy import select, func, desc
+from sqlalchemy import update, select, func, desc
 
 from dataengine import Context
 from dataengine.common.util import days_ago_datetime
 from dataengine.model.dao.event import Event
+
+
+def get_event(note_id):
+    stmt = select(Event).filter(Event.id == note_id)
+
+    with Context.db_session() as session:
+        events = session.execute(stmt).scalars().all()
+
+    return events[0] if events else None
 
 
 def get_events_since(user_id, days_ago) -> typing.List[Event]:
@@ -22,11 +31,37 @@ def get_events_since(user_id, days_ago) -> typing.List[Event]:
 
 
 def put_event(user_id: str, args: typing.Dict[str, str]):
+    event = _args_to_event(user_id, args)
+
+    with Context.db_session() as session:
+        session.add(event)
+        session.commit()
+
+
+def update_event(user_id, args):
+    event = _args_to_event(args, user_id)
+    stmt = (update(Event)
+        .where(Event.id == event.id)
+        .values(
+        user_id=event.user_id,
+        body=event.body,
+        activity=event.activity,
+        duration=event.duration,
+        time=event.time,
+        feel=event.feel)
+    )
+    with Context.db_session() as session:
+        session.execute(stmt)
+        session.commit()
+
+
+def _args_to_event(user_id, args) -> Event:
+    event_id = args.get('id', uuid.uuid4())
     time = args.get('time')
     duration = args.get('duration')
 
-    event = Event(
-        id=uuid.uuid4(),
+    return Event(
+        id=event_id,
         user_id=user_id,
         body=args['body'],
         activity=args.get('activity'),
@@ -34,7 +69,3 @@ def put_event(user_id: str, args: typing.Dict[str, str]):
         time=time if time else func.now(),
         feel=args.get('feel'),
     )
-
-    with Context.db_session() as session:
-        session.add(event)
-        session.commit()
