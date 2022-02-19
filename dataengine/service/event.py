ede330a2f5c2385fafdaaadbae81aed3f3412c27
Maros Korinek <apmaros@gmail.com>
2022-02-19 11:19:35 +0000

    Add timeline to events

--- dataengine/service/event.py
+++ dataengine/service/event.py
@@ -8,7 +8,7 @@ def group_events_by_date(events: t.List[Event]) -> t.Dict[str, Event]:
     grouped: t.Dict[str, ItemGroup] = {}
 
     for event in events:
-        date = event.time.strftime('%d-%m-%Y')
+        date = event.time.strftime('%a, %d-%m-%Y')
         if date not in grouped:
             grouped[date] = ItemGroup(date, [])
         grouped[date].value.append(event)
