a506eb8ffef9a1cbf17a461363e1d64801f950bb
Maros Korinek <apmaros@gmail.com>
2022-03-06 11:03:27 +0000

    Minor cleanup

--- dataengine/main.py
+++ dataengine/main.py
@@ -3,6 +3,10 @@ from dataengine.common.log import logger
 
 
 def start_app():
+    """
+    Starts the Application
+    DO NOT USE FOR PRODUCTION ENVIRONMENT
+    """
     logger.info("Starting Data Engine app")
     app.run()
     logger.info("Started Data Engine app")
--- dataengine/service/model/service_response.py
+++
@@ -1,9 +0,0 @@
-import typing as t
-from dataclasses import dataclass
-
-
-@dataclass
-class ServiceResponse:
-    result: t.Any
-    success: bool
-    error_msg: str
